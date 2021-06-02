19e0ddeb1663118eb4d9e27860aa35e9fbbd8632
gabriellramos <gabrielramosnascimento195@gmail.com>
2021-06-02 11:20:27 -0400

    final

--- questao11.c
+++ questao11.c
@@ -1,19 +1,26 @@
 //questao 11
 #include <stdio.h>
 #include <stdlib.h>
-#define N 5
+#define N 3
 
 typedef struct{
 	char nome[15];
 	float potencia; //em kW
 	float tempoAtivoPorDia; //em Horas
+	
 }Eletrodomestico;
 
-void salva(Eletrodomesto *aparelhos);
+void salva(Eletrodomestico *aparelhos);
 void consumoEletrico(Eletrodomestico *aparelhos, int t); //t -> tempo em dias
 
 int main(){
-	// jhdbkfj
+	Eletrodomestico eletrodomesticos[N];
+	int tempo;
+	salva(eletrodomesticos);
+	printf ("\n\nInsira o tempo em dias de consumo dos aparelhos: ");
+	scanf ("%d", &tempo);
+	consumoEletrico(eletrodomesticos, tempo);
+	
 	return 0;
 }
 
@@ -24,8 +31,8 @@ void salva(Eletrodomestico *aparelhos){
 		printf ("Eletrodomestico %d\n", i+1);
 		printf ("Nome: ");
 		fflush (stdin);
-		fgets (aparelhos[i].nome);
-		printf ("Potencia: ");
+		fgets (aparelhos[i].nome, 15, stdin);
+		printf ("Potencia (em kW): ");
 		scanf ("%f", &aparelhos[i].potencia);
 		printf ("Tempo ativo por dia(horas): ");
 		scanf ("%f", &aparelhos[i].tempoAtivoPorDia);
@@ -34,5 +41,18 @@ void salva(Eletrodomestico *aparelhos){
 
 void consumoEletrico(Eletrodomestico *aparelhos, int t){//t -> tempo em dias
 	int i;
-	float consulmoRelativo[N], somaConsulmo=0.0,
+	float consumoRelativo[N], somaConsumo=0.0, consumoTotal;
+	
+	for (i=0;i<N;i++){
+		consumoRelativo[i] = (aparelhos[i].potencia * aparelhos[i].tempoAtivoPorDia) * t;
+		somaConsumo += consumoRelativo[i];
+	}
+	
+	consumoTotal = somaConsumo;
+	printf ("Consumo Total da Casa: %.2fkW\n\n",consumoTotal);
+	for (i=0; i<N; i++){
+		consumoRelativo[i] = (consumoRelativo[i]/consumoTotal)*100;
+		printf ("Aparelho: %s", aparelhos[i].nome);
+		printf ("Consumo relativo: %.2f%%\n", consumoRelativo[i]);
+	}
 }
--- questao15.c
+++ questao15.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define N 2
+#define N 1
 
 typedef struct {
 	char nome[50];
@@ -11,8 +11,10 @@ typedef struct {
 }Questionario;
 
 void cadastrarNovaEntrevista(Questionario *entrevista, int indice);
-int pesquisaQuestionarioPorNome(char *nome);
-void percHomensMulheres(Questionario *entrevista, float *percentuais);
+int pesquisaQuestionarioPorNome(Questionario *entrevista, char *nome);
+void imprimeQuestionario(Questionario *entrevista, int indice);
+float percHomens(Questionario *entrevista);
+float percMulheres(Questionario *entrevista);
 float percIdade18a40anos(Questionario *entrevista);
 float percFumantes(Questionario *entrevista);
 float rendaMediaEntrevistados(Questionario *entrevista);
@@ -21,21 +23,37 @@ void mudaUltimoCaractereDaString(char *str, int tam);
 int main(){
 	int i;
 	Questionario entrevista[N];
-	float percHeM[2], percPde18a40, percFuma, rendaMedia;
+	float percH, percM,percPde18a40, percFuma, rendaMedia;
+	char nome[50];
+	
 	for (i=0; i<N; i++){
 		cadastrarNovaEntrevista(entrevista, i);
+		printf ("\n\n");
+		imprimeQuestionario(entrevista, i);
 	}
 	
-	percHomensMulheres(entrevista);
+	percM = percMulheres(entrevista);
+	percH = percHomens(entrevista);
+
 	percPde18a40 = percIdade18a40anos(entrevista);
 	percFuma = percFumantes(entrevista); 
 	rendaMedia = rendaMediaEntrevistados(entrevista);
 	
-	printf ("Estatisticas sobre as entrevistas: \n");
-	printf ("Percentual de homens e mulheres: %.2f%% - H || %.2f%% - M\n", percHeM[0], percHeM[1]); //0 - homens ---- 1 - mulheres
+	printf ("\n\n\nEstatisticas sobre as entrevistas: \n");
+	printf ("Percentual de homens e mulheres: %.2f%% - H || %.2f%% - M\n", percH, percM); //0 - homens ---- 1 - mulheres
 	printf ("Percentual de pessoas com 18 a 40 anos: %.2f%%\n", percPde18a40);
 	printf ("Pencentual de fumantes: %.2f%%\n", percFuma);
 	printf ("Renda media dos entrevistados: R$ %.2f\n", rendaMedia);
+	
+	printf ("\nInsira um nome para pesquisar o questionario: ");
+	fflush (stdin);
+	fgets (nome, 50, stdin);
+	
+	int pos = pesquisaQuestionarioPorNome(entrevista,nome);
+	
+	if (pos != -1)
+		imprimeQuestionario(entrevista, pos);
+	else ("questionario nao encontrado\n");
 	return 0;
 }
 
@@ -50,9 +68,9 @@ void cadastrarNovaEntrevista(Questionario *entrevista, int indice){
 		printf ("Sexo (1 - Feminino / 2 - Masculino / 3 - Outro): ");
 		scanf ("%d", &entrevista[indice].sexo);
 		
-		if (entrevista[indice].sexo>=1 && entrevista[indice].sexo<4) break;
+		if (entrevista[indice].sexo>=1 && entrevista[indice].sexo<=4) break;
 	}
-	
+
 	printf ("Idade: ");
 	scanf ("%d", &entrevista[indice].idade);
 	printf ("Renda Mensal: R$ ");
@@ -82,28 +100,102 @@ void cadastrarNovaEntrevista(Questionario *entrevista, int indice){
 	
 }
 
-int pesquisaQuestionarioPorNome(char *nome){
-	char strNome[15], str[15];
-	strcpy()
+int pesquisaQuestionarioPorNome(Questionario *entrevista, char *nome){
+	char strNome[50], str[50];
+	int i, pos = -1;
+
+	mudaUltimoCaractereDaString(nome, strlen(nome));
+
+	strcpy(strNome, nome);
+	strupr(strNome);
+	
+	for (i=0; i<N;i++){
+		strcpy(str, entrevista[i].nome);
+		strupr(str);
+		if (strcmp(str, strNome) == 0){
+			pos = i;
+		}
+	}
+	return pos;
 }
 
-void percHomensMulheres(Questionario *entrevista, float *percentuais){
+void imprimeQuestionario(Questionario *entrevista, int indice){
+	char genero[10], fuma[4], gostaEsporte[4],praticaAtividade[4];
+
+	if (entrevista[indice].sexo==1) strcpy(genero,"Feminino");
+	else if (entrevista[indice].sexo==2)  strcpy(genero,"Masculino");
+	else strcpy(genero,"Outro");
+
+	strcpy(fuma,entrevista[indice].fumante == 1 ? "Sim" : "Nao");
+	strcpy(gostaEsporte,entrevista[indice].gostaDeEsporte == 1 ? "Sim" : "Nao");
+	strcpy(praticaAtividade,entrevista[indice].praticaAtividadeFisica == 1 ? "Sim" : "Nao");
 	
+	printf ("Entrevista %d\n", indice+1);
+	printf ("Nome: %s\n", entrevista[indice].nome);
+	printf ("Sexo: %s\n", genero);
+	printf ("Idade: %d\n", entrevista[indice].idade);
+	printf ("Renda Mensal: R$ %.2f\n", entrevista[indice].rendaMensal);
+	printf ("Fumante?: %s\n", fuma);
+	printf ("Gosta de Esporte?: %s\n", gostaEsporte);
+	printf ("Pratica Atividade Fisica?: %s\n\n", praticaAtividade);
+}
+
+float percHomens(Questionario *entrevista){
+	int i, homens=0;
+	
+	//1 - feminino, 2 - masculino
+	for (i = 0; i<N; i++){
+		if(entrevista[i].sexo == 2)
+			homens = homens + 1;
+	}
+	
+	return ((float)homens/N)*100;
+}
+
+
+float percMulheres(Questionario *entrevista){
+	
+	int i, mulheres=0;
+	//1 - feminino, 2 - masculino
+	for (i = 0; i<N; i++){
+		if(entrevista[i].sexo==1)
+			mulheres = mulheres + 1;
+	}
+	
+	return ((float)mulheres/N)*100;
 }
 
 float percIdade18a40anos(Questionario *entrevista){
+	int i, cont=0;
 	
+	for (i=0; i<N; i++){
+		if (entrevista[i].idade>=18 && entrevista[i].idade<=40)
+			cont++;
+	}
+	
+	return (cont/N) * 100;
 }
 
 float percFumantes(Questionario *entrevista){
+	int i, cont=0;
+	for (i=0;i<N; i++)
+		if (entrevista[i].fumante==1)
+			cont++;
 	
+	return ((float)cont/N)*100;
 }
 
 float rendaMediaEntrevistados(Questionario *entrevista){
-	
+	float soma=0.0;
+	int i;
+	for (i=0;i<N;i++){
+		soma+=entrevista[i].rendaMensal;
+	}
+	return soma/N;
 }
 
 //funcao para ajudar na pesquisa por nome
 void mudaUltimoCaractereDaString(char *str, int tam){
-	str[tam-1] = '\0';	
+	if (str[tam-1]=='\n')
+		str[tam-1] = '\0';	
 }
